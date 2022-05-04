fdbf56e8edab6cacb4f92872a49b18cac4bc674d
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-04 15:09:15 -0500

    Update freecodecampApper.js

--- freecodecampApper.js
+++ freecodecampApper.js
@@ -1,10 +1,9 @@
-
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el");
 let count = 0;
 function increment() {
     count++;
-    countEl.innerText = count
+    countEl.textContent = count
     console.log("The button was clicked");    
 }
 
@@ -12,6 +11,7 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr // changed for innerText to deploy the space 
+    countEl.textContent = 0
+    count = 0
     console.log(count);
 }
-
